7c85cb4b39cb84d6cb20bc25214fede6b7c99afd
OctavioNovelo <octavionovelo22@gmail.com>
2024-08-20 10:50:51 -0600

    Coments Correction

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-''' Description: type-annotated function add that takes a float a and a float b
+
+
+'''Description: type-annotated function add that takes a float a and a float b
                  as arguments and returns their sum as a float
     Arguments: a: float
                 b: float
