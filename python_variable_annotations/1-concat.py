70e39e62b64c1d86f4ccffac367dec03a0acacde
OctavioNovelo <octavionovelo22@gmail.com>
2024-08-20 11:01:56 -0600

    Task2

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+'''Description: type-annotated function concat that takes a string str1 and a string str2
+                 as arguments and returns a concatenated string
+    Arguments: str1: str
+                str2: str
+'''
+
+def concat(str1: str, str2: str) -> str:
+    '''return a concatenated string'''
+    return(str1, str2)
