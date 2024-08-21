2de5f56e2d9cae1ed4b887145ede69f90fa85e98
OctavioNovelo <octavionovelo22@gmail.com>
2024-08-21 16:50:34 -0500

    Changes

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+'''Description: The function take a float like and argument and return
+                the floor of that float
+    Arguments: a flaot
+    '''
+def floor(a: float)
+    
