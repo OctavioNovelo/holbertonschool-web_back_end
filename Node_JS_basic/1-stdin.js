b1a186c7f36e3fa6cc9df418cdb339ef761294f9
OctavioNovelo <octavionovelo22@gmail.com>
2024-09-26 15:09:38 -0500

    Correciton

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,9 @@
 console.log('Welcome to Holberton School, what is your name?');
-
-
 process.stdin.on('data', (data) => {
-  const name = data.toString().trim(); 
-  console.log(`Your name is: ${name}`);
-
-  console.log('This important software is now closing');
-  process.exit();
+    const name = data.toString().trim();
+    console.log(`Your name is: ${name}`);
+    if (process.stdout.isTTY) {
+        console.log('This important software is now closing');
+    }
+    process.exit();
 });
