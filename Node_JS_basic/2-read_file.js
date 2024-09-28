ed860d829fd2df6c3a87adcfdf7a0c4da58c9efa
OctavioNovelo <octavionovelo22@gmail.com>
2024-09-28 12:36:16 -0500

    Correction

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,37 +1,25 @@
 const fs = require('fs');
-
 function countStudents(path) {
   try {
-
     const data = fs.readFileSync(path, 'utf-8').trim();
-
-
     const lines = data.split('\n').filter(line => line !== '');
-    
-
     if (lines.length <= 1) {
       console.log('Number of students: 0');
       return;
     }
-
     const headers = lines[0].split(',');
     const students = lines.slice(1);
-
     console.log(`Number of students: ${students.length}`);
-
     const fields = {};
-
     students.forEach((student) => {
       const studentData = student.split(',');
       const firstName = studentData[0];
       const field = studentData[3];
-      
       if (!fields[field]) {
         fields[field] = [];
       }
       fields[field].push(firstName);
     });
-
     for (const [field, names] of Object.entries(fields)) {
       console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
     }
@@ -39,5 +27,4 @@ function countStudents(path) {
     throw new Error('Cannot load the database');
   }
 }
-
 module.exports = countStudents;
