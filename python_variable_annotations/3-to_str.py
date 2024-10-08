d1f5e1ab5aa5cc55f1d5d2fe26c3644f3140e386
OctavioNovelo <octavionovelo22@gmail.com>
2024-10-08 14:05:16 -0600

    Correction

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""
+Este script contiene una función para convertir un número flotante en su representación de cadena.
+"""
 
 def to_str(n: float) -> str:
+    """
+    Convierte un número de punto flotante a una cadena de texto.
+
+    Args:
+        n (float): El número de punto flotante a convertir.
+
+    Returns:
+        str: La representación en cadena del número flotante.
+    """
     return str(n)
