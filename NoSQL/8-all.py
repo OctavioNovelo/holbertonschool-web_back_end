24daecf7f800fd2a217ae8d170f2c29e26a87aa2
OctavioNovelo <octavionovelo22@gmail.com>
2024-10-08 14:21:45 -0600

    Correction?

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
-""" Write a Python function that lists all documents in a collection """
+"""Module that defines a function to list all documents in a MongoDB collection."""
+
+from pymongo.collection import Collection
 
 
-def list_all(mongo_collection):
-    """mongo_collection will be the pymongo collection object
-       Return an empty list if no document in the collection
+def list_all(mongo_collection: Collection):
+    """
+    Lists all documents in the given MongoDB collection.
+    
+    Args:
+        mongo_collection (Collection): The MongoDB collection to list documents from.
+    
+    Returns:
+        list: A list of all documents in the collection. 
+              Returns an empty list if no documents are found.
     """
     documents = mongo_collection.find()
     if documents.count() == 0:
         return []
-    return documents
+    return list(documents)
+
+
+if __name__ == "__main__":
+    # Código para prueba si es necesario, pero este bloque no se ejecutará al importar el módulo
+    pass
