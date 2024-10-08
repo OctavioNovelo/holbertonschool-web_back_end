5c23903b20bfd0269de32fbc5a582689325e792c
OctavioNovelo <octavionovelo22@gmail.com>
2024-10-08 13:54:30 -0600

    Correction

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 
-'''Description: type-annotated function concat that takes a string str1 and a string str2
+'''Description: type-annotated function concat that takes a 
+    string str1 and a string str2
                  as arguments and returns a concatenated string
     Arguments: str1: str
                 str2: str
 '''
 
-def concat(str1: str, str2: str) -> str:
+
+def concat (str1: str, str2: str) -> str:
     '''return a concatenated string'''
-    return(str1 + str2)
+    return (str1 + str2)
